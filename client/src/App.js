cf24212ffeedde3a9897cef88ef1d26447cd523e
30sweetener09 <vuongan3467@gmail.com>
2025-08-15 16:13:34 +0700

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World!
+      <p>Welcome to my React application.</p>
+      <button onClick={() => alert('Button clicked!')}>Click Me</button>
+    </div>
+  );
+}
+
+export default App;
